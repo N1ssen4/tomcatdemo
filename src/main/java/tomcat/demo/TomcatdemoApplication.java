c9f9771d71adc3257f232d07ec18bf792605b075
Mads <mads.rosenlund@hotmail.com>
2021-02-19 14:50:42 +0100

    Initial commit

---
+++ src/main/java/tomcat/demo/TomcatdemoApplication.java
@@ -0,0 +1,13 @@
+package tomcat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tomcat/demo/TomcatdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tomcat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
